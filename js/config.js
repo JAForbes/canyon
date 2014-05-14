524017105f99bdd0de8914c2fe31a1cb4d56ec9c
James <james.a.forbes@gmail.com>
2014-05-14 13:27:03 +1000

    Attempt to move my specific data to config file.
    Signed-off-by: James <james.a.forbes@gmail.com>

---
+++ js/config.js
@@ -0,0 +1,26 @@
+config = {
+	heading: 'Canyon',
+	tumblr: 'canyongames',
+	twitter: 'james_a_forbes',
+	profile: 'img/profile.jpeg',
+	description: [
+		'Programmer and musician.',
+		'I write; code, music and stories.',
+		'I live in Melbourne.',
+	],
+	/*
+		Links other than twitter,tumblr ...whatever you want.
+	*/
+	otherHTML: [
+		'Check out some of my <a href="http://canyon.itch.io/">games.</a>'
+	],
+	css: { 
+			backgroundColor: '#AABBDD',
+			color: 'white',
+	},
+	options: {
+		//automatically prettify code <false or {urlArgs} >
+		//see https://code.google.com/p/google-code-prettify/wiki/GettingStarted
+		prettify: { lang: 'javascript', skin: 'sunburst' }
+	}
+}
---
+++ js/setup.js
@@ -0,0 +1,57 @@
+(function(config){
+	
+	function tag(tag){
+		return function(attr,content){
+			content = content || ''
+			content = content.join && content.join('') || content
+			return [
+				'<',tag,attributes(attr),'>',
+					content,
+				'</',tag,'>'
+			].join('')
+		}
+	}
+
+	function attributes(attr){
+		return ' ' + _(attr).map(function(value,key){
+		  return key+'='+'"'+value+'"'
+		}).join(' ')
+	}
+
+	var div = tag('div'), h1 = tag('h1'), img = tag('img'), p = tag('p'), a = tag('a'), script = tag('script');
+	
+
+	templates = {
+		
+		sidebar:
+			div({class:'intro'},
+				h1({},config.heading)+
+				img({class:"profile",src:config.profile})+
+				div({id:'blurb'},
+					[
+						p({},config.description.join('</p><p>')),
+						a({href:'https://twitter.com/'+config.twitter},
+							img({class:'icon', src:'img/tumblr_white.png'})
+						),
+						a({href:'https://canyongames.tumblr.com/'+config.twitter},
+							img({class:'icon', src:'img/tumblr_white.png'})
+						),
+						p({},config.otherHTML)
+					]
+				)
+			)
+		,
+		//tumblr: (function(){
+			//TODO: use tumblr api so config can alter live which blog to pull from
+		//}(),
+			
+
+
+		prettify: (function(){
+			//'<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=sunburst"></script>'
+		})()
+	}
+
+})(config)
+
+$('body').append(templates.sidebar)
