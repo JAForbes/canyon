dc37840a6866f6aec84ed2cbb4a2a50584abbc08
James <james.a.forbes@gmail.com>
2014-05-18 17:50:43 +1000

    Added external temple.js

--- js/setup.js
+++ js/setup.js
@@ -1,44 +1,26 @@
 (function(config){
-	
-	function tag(tag){
-		return function(attr,content){
-			content = content || ''
-			content = content.join && content.join('') || content
-			return [
-				'<',tag,attributes(attr),'>',
-					content,
-				'</',tag,'>'
-			].join('')
-		}
-	}
-
-	function attributes(attr){
-		return ' ' + _(attr).map(function(value,key){
-		  return key+'='+'"'+value+'"'
-		}).join(' ')
-	}
-
-	var div = tag('div'), h1 = tag('h1'), img = tag('img'), p = tag('p'), a = tag('a'), script = tag('script');
-	
 
 	templates = {
 		
 		sidebar:
 			div({class:'intro'},
-				h1({},config.heading)+
-				img({class:"profile",src:config.profile})+
-				div({id:'blurb'},
-					[
-						p({},config.description.join('</p><p>')),
-						a({href:'https://twitter.com/'+config.twitter},
-							img({class:'icon', src:'img/twitter_white.png'})
-						),
-						a({href:'https://canyongames.tumblr.com/'+config.twitter},
-							img({class:'icon', src:'img/tumblr_white.png'})
-						),
-						p({},config.otherHTML)
-					]
-				)
+				[
+					h1(config.heading),
+					img({class:"profile",src:config.profile},''),
+					div({id:'blurb'},
+						[
+							p(config.description.join('</p><p>')),
+							a({href:'https://twitter.com/'+config.twitter},
+								img({class:'icon', src:'img/twitter_white.png'},'')
+							),
+							a({href:'https://canyongames.tumblr.com/'+config.tumblr},
+								img({class:'icon', src:'img/tumblr_white.png'},'')
+							),
+
+							p(config.otherHTML)
+						]
+					)
+				]
 			)
 		,
 		//tumblr: (function(){
